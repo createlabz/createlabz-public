9b17d9f1f114f4093edd837e33360c639a65166a
niclabios1 <73338072+niclabios1@users.noreply.github.com>
2020-10-23 23:44:48 +0800

    Create PotentioDimmer.ino
    A simple Arduino project that makes use of a potentiometer, LED, and a resistor. This project controls LED brightness using a potentiometer.

---
+++ arduino-upgradedStarterKit/PotentioDimmer/PotentioDimmer.ino
@@ -0,0 +1,20 @@
+const int POT = A0; //analog input pin for potentiometer
+const int LED = 9; //input pin for LED
+
+int POTvalue=0; //variable for input values from A0
+int outPOTvalue=0; //variable for output value from potentio
+
+void setup() {
+  //Initialize pinModes
+  pinMode (LED,OUTPUT);
+  pinMode (POT,INPUT);
+
+}
+
+void loop() {
+  POTvalue = analogRead(POT); //reads value from analog pin
+  outPOTvalue = map(POTvalue,0,1023,0,255); //re-map from 0-1023 to 0-250
+  
+  analogWrite(LED,outPOTvalue);
+  delay(1000);
+}
